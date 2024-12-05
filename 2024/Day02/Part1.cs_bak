a44008ca894b7f19379079e75053e25eb1e82ae2
Eric Botter <eric.botter@gmail.com>
2024-12-05 19:22:46 +0100

    Failed attempt at recursive solution

---
+++ 2024/Day02/Part1.cs_bak
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+var numberRegex = NumberRegex();
+
+var lines = File.ReadAllLines("input.txt");
+
+var reports = lines.Select(line => numberRegex.Matches(line).Select(match => int.Parse(match.Value)).ToList());
+
+var sum = 0;
+foreach (var report in reports)
+{
+    List<int> diffs = [];
+    for (var i = 1; i < report.Count; i++)
+    {
+        diffs.Add(report[i] - report[i - 1]);
+    }
+
+    sum += diffs.All(x => x is <= -1 and >= -3) || diffs.All(x => x is >= 1 and <= 3) ? 1 : 0;
+}
+
+Console.WriteLine(sum);
+
+partial class Program
+{
+    [GeneratedRegex("[0-9]+")]
+    private static partial Regex NumberRegex();
+}
--- 2024/Day02/Program.cs
+++ 2024/Day02/Program.cs
@@ -7,18 +7,43 @@ var lines = File.ReadAllLines("input.txt");
 var reports = lines.Select(line => numberRegex.Matches(line).Select(match => int.Parse(match.Value)).ToList());
 
 var sum = 0;
+var i = 1;
 foreach (var report in reports)
 {
-    List<int> diffs = [];
-    for (var i = 1; i < report.Count; i++)
-    {
-        diffs.Add(report[i] - report[i - 1]);
-    }
-
-    sum += diffs.All(x => x is <= -1 and >= -3) || diffs.All(x => x is >= 1 and <= 3) ? 1 : 0;
+    var reportValid = IsReportValid(report);
+    Console.WriteLine($"Report {i++} is valid: {reportValid}");
+    sum += reportValid ? 1 : 0;
 }
 
 Console.WriteLine(sum);
+return;
+
+
+bool IsReportValid(List<int> report)
+    => IsRecursiveReportValid(report, false, true) || IsRecursiveReportValid(report, true, true);
+
+
+bool IsRecursiveReportValid(List<int> report, bool increasing, bool errorTolerated)
+{
+    if (report.Count <= 1)
+        return true;
+
+    if (increasing)
+    {
+        if (report[1] - report[0] is >= 1 and <= 3)
+            return IsRecursiveReportValid(report[1..], increasing, errorTolerated);
+    }
+    else
+    {
+        if (report[0] - report[1] is >= 1 and <= 3)
+            return IsRecursiveReportValid(report[1..], increasing, errorTolerated);
+    }
+
+    if (!errorTolerated) return false;
+
+    return IsRecursiveReportValid(report[1..], increasing, false)
+        || IsRecursiveReportValid(report[2..].Prepend(report[0]).ToList(), increasing, false);
+}
 
 partial class Program
 {
