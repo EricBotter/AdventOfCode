f8f0e4c7b0256aac3716adfd0bf216371fd22b1d
Eric Botter <eric.botter@gmail.com>
2024-12-06 17:53:53 +0100

    Day 06 Part 2 Naive

--- 2024/Day06/Program.cs
+++ 2024/Day06/Program.cs
@@ -1,4 +1,4 @@
-﻿Dictionary<Direction, Direction> directionMap = new Dictionary<Direction, Direction>
+﻿var directionMap = new Dictionary<Direction, Direction>
 {
     { Direction.North, Direction.East },
     { Direction.East, Direction.South },
@@ -13,47 +13,68 @@ var sizeY = lines.Length;
 
 var map = new char[sizeX, sizeY];
 
-var position = new Location(0, 0);
-var direction = Direction.North;
+
+var initialPosition = new Location(0, 0);
 for (var i = 0; i < sizeY; i++)
 for (var j = 0; j < sizeX; j++)
 {
     map[j, i] = lines[i][j];
     if (map[j, i] == '^')
     {
-        position = new Location(j, i);
+        initialPosition = new Location(j, i);
         map[j, i] = 'X';
     }
 }
 
-while (!position.IsOob(sizeX, sizeY))
-{
-    var checkPosition = position.Move(direction);
-
-    if (!checkPosition.IsOob(sizeX, sizeY) && map.At(checkPosition) == '#')
-    {
-        direction = directionMap[direction];
-        continue;
-    }
-
-    position = checkPosition;
-    if (!position.IsOob(sizeX, sizeY))
-    {
-        map.At(position) = 'X';
-    }
-}
-
 var sum = 0;
-for (var i = 0; i < sizeY; i++)
-for (var j = 0; j < sizeX; j++)
+
+for (var i = 0; i < sizeX; i++)
+for (var j = 0; j < sizeY; j++)
 {
-    if (map[j, i] == 'X')
-    {
+    Console.WriteLine($"testing {i}, {j}");
+
+    if (map[i, j] != '.')
+        continue;
+
+    map[i, j] = '#';
+
+    if (Run())
         sum++;
-    }
+
+    map[i, j] = '.';
 }
 
 Console.WriteLine(sum);
+
+
+bool Run()
+{
+    var knownLocations = new MultiValueDictionary<Location, Direction>();
+    var position = initialPosition;
+    var direction = Direction.North;
+
+    while (!position.IsOob(sizeX, sizeY))
+    {
+        var checkPosition = position.Move(direction);
+
+        if (knownLocations.ContainsKey(checkPosition) && knownLocations[checkPosition].Contains(direction))
+            return true;
+
+        if (!checkPosition.IsOob(sizeX, sizeY) && map.At(checkPosition) == '#')
+        {
+            direction = directionMap[direction];
+            continue;
+        }
+
+        position = checkPosition;
+        if (!position.IsOob(sizeX, sizeY))
+        {
+            knownLocations.Add(position, direction);
+        }
+    }
+
+    return false;
+}
 
 enum Direction
 {
@@ -77,6 +98,17 @@ record Location(int X, int Y)
         };
 
     public bool IsOob(int maxX, int maxY) => X < 0 || Y < 0 || X >= maxX || Y >= maxY;
+}
+
+public class MultiValueDictionary<Key, Value> : Dictionary<Key, List<Value>> {
+    public void Add(Key key, Value value) {
+        List<Value> values;
+        if (!this.TryGetValue(key, out values)) {
+            values = new List<Value>();
+            this.Add(key, values);
+        }
+        values.Add(value);
+    }
 }
 
 static class Exts
