58eb5ad4e3105b21f6fb1be569abb039d3b6f336
Eric Botter <eric.botter@gmail.com>
2024-12-08 11:45:40 +0100

    Day 07 Part 1

---
+++ 2024/Day07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+Func<string, List<long>> parseLineToNumberList =
+    line => NumberRegex().Matches(line).Select(match => long.Parse(match.Value)).ToList();
+
+var calibrations = File.ReadAllLines("input.txt").Select(parseLineToNumberList).Select(Calibration.FromInts);
+
+var sum = calibrations.Where(calibration => calibration.IsValid()).Sum(calibration => calibration.Target);
+
+Console.WriteLine(sum);
+
+record Calibration(long Target, List<long> Operands)
+{
+    public static Calibration FromInts(List<long> values) => new(values[0], values[1..]);
+
+    public bool IsValid()
+    {
+        return InternalIsValid(Operands[0], Operands[1..]);
+
+        bool InternalIsValid(long currentValue, List<long> remainingValues)
+        {
+            if (currentValue > Target)
+                return false;
+            if (remainingValues.Count == 0)
+                return currentValue == Target;
+
+            return InternalIsValid(currentValue + remainingValues[0], remainingValues[1..])
+                   || InternalIsValid(currentValue * remainingValues[0], remainingValues[1..]);
+        }
+    }
+}
+
+partial class Program
+{
+    [GeneratedRegex("[0-9]+")]
+    private static partial Regex NumberRegex();
+}
