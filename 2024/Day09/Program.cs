bef27ec71208d44cf3d539e350643246f4d3f581
Eric Botter <eric.botter@gmail.com>
2024-12-13 10:31:38 +0100

    Day 09 Part 2

--- 2024/Day09/Program.cs
+++ 2024/Day09/Program.cs
@@ -1,51 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using System.Collections;
 
 var line = File.ReadAllText("input.txt").Trim().Select(c => int.Parse(c.ToString())).ToList();
 
 var zippedLine = line.Chunk(2);
 
-var drive = new List<int>();
+var drive = new List<DiskFile>();
+var gaps = new List<Gap>();
+
 var id = 0;
+var position = 0;
 foreach (var chunk in zippedLine)
 {
-    for (var i = 0; i < chunk[0]; i++)
-    {
-        drive.Add(id);
-    }
+    drive.Add(new DiskFile(id, position, chunk[0]));
+
+    position += chunk[0];
 
     if (chunk.Length == 2)
     {
-        for (var i = 0; i < chunk[1]; i++)
-        {
-            drive.Add(-1);
-        }
+        gaps.Add(new Gap(position, chunk[1]));
+        position += chunk[1];
     }
 
     id++;
 }
 
-var rightIdx = drive.Count - 1;
-for (var leftIdx = 0; rightIdx > leftIdx; leftIdx++)
+for (var i = drive.Count - 1; i >= 0; i--)
 {
-    if (drive[leftIdx] == -1)
+    var file = drive[i];
+    for (var j = 0; j < gaps.Count; j++)
     {
-        drive[leftIdx] = drive[rightIdx--];
-        while (drive[rightIdx] == -1)
+        var gap = gaps[j];
+        if (gap.Length >= file.Length)
         {
-            rightIdx--;
+            drive[i] = file with { Position = gap.Position };
+            gaps[j] = new Gap(gap.Position + file.Length, gap.Length - file.Length);
+            break;
+        }
+
+        if (gaps[j].Position >= file.Position)
+        {
+            break;
         }
     }
 }
 
-drive = drive[..(rightIdx+1)];
-
 var sum = 0L;
-for (var i = 0; i < drive.Count; i++)
+foreach (var file in drive)
 {
-    if (drive[i] != -1)
+    for (var i = 0; i < file.Length; i++)
     {
-        sum += drive[i] * i;
+        sum += file.Id * (file.Position + i);
     }
 }
 
+
+
 Console.WriteLine(sum);
+
+record DiskFile(int Id, int Position, int Length);
+record Gap(int Position, int Length);
